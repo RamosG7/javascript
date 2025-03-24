48246c86d17d208a075f2de7ca099860566953fe
Gabriel Ramos <ramosgabriel662@gmail.com>
2025-03-24 19:34:07 -0300

    Condicionais PT I

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if( vel > 60){//condição simples, pois não há um else
+    console.log('Você ultrapassou a velocidade permitida. (multado)')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`vivendo em ${país}`)
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
